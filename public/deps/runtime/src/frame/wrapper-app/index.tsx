4f4867ba9c514d4fe82843de0ae32effa1bf8d7c
dankovk <dankov.kiev@gmail.com>
2025-10-24 21:54:42 +0300

    proper exports

--- public/deps/runtime/src/frame/wrapper-app/index.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @hypertool/runtime/frame/wrapper-app
- *
- * React-based wrapper application for the HyperFrame sandbox
- */
-
-export { WrapperApp } from './WrapperApp';
-export type { WrapperAppProps, ControlsPanelProps, SandboxContainerProps } from './types';
-export { CanvasProvider, useCanvas } from './context';
